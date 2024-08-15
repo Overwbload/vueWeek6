8aed30eaf3b359d943716fe1a545fba89237c8e6
Jason <jie38863558@gmail.com>
2024-08-15 20:03:27 +0800

    Updates

---
+++ assets/HomeView-TIY-IBUM.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as o,a as t}from"./index-Bv1ygyXE.js";const c={},a={class:"home"},n=t("h1",{class:"text-center"},"Hello, This is Home Page.",-1),_=[n];function r(i,d,l,p,h,m){return s(),o("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
